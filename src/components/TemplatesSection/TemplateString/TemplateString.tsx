9dc1d26e3e67a9b8b5c3143d7c5aca0ab550f3d6
Yerichev Yegor <yegor.yerichev@nexign.com>
2023-10-10 16:28:21 +0300

    add template plugin function

--- src/components/TemplatesSection/TemplateString/TemplateString.tsx
+++ src/components/TemplatesSection/TemplateString/TemplateString.tsx
@@ -1,11 +1,31 @@
-import style from './index.module.css';
+import style from './index.module.css'
 interface Props {
-  label: string;
-  value: string;
+  label: string
 }
-export const TemplateString = ({ label }: Props) => (
-  <div>
-    <span className={style.test}>{label}</span>
-    <button>+</button>
-  </div>
-);
+export const TemplateString = ({ label }: Props) => {
+  const addTemplate = (field_type: string) => () => {
+    // serialize command as text
+    const sScript = `var oDocument = Api.GetDocument();
+    var oParagraph = Api.CreateParagraph();
+    var oParagraph = Api.CreateParagraph();
+    oParagraph.AddText('"${field_type}"');
+    var oParagraph = Api.CreateParagraph();
+    oParagraph.AddText('" + field_type + "');
+    oDocument.InsertContent([oParagraph]);
+    `
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    window.Asc.plugin.info.recalculate = true
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    window.Asc.plugin.executeCommand('command', sScript)
+  }
+
+  return (
+    <div className={style.templateStringWrapper}>
+      <span>{label}</span>
+      <button onClick={addTemplate(label)}>+</button>
+    </div>
+  )
+}
--- src/components/TemplatesSection/index.tsx
+++ src/components/TemplatesSection/index.tsx
@@ -1,10 +1,13 @@
-import { TemplateString } from './TemplateString/TemplateString';
+import { TemplateString } from './TemplateString/TemplateString'
 
 export const TemplatesSection = () => {
   return (
     <div>
       Templates
-      <TemplateString label="test" value="test" />
+      <TemplateString label="test" />
+      <TemplateString label="test1" />
+      <TemplateString label="test2" />
+      <TemplateString label="test3" />
     </div>
-  );
-};
+  )
+}
